f72d76eab7d5ef250daa519885b6eab6fd131a7b
Kien Nguyen <kiennd29@gmail.com>
2016-07-05 08:18:33 +0700

    Update code

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secret': 'TodayYou-TomorrowOur',
-    'database': 'mongodb://privatesshmanager:privatesshmanager@ds013545.mlab.com:13545/privatesshserver'
+    //'database': 'mongodb://privatesshmanager:privatesshmanager@ds013545.mlab.com:13545/privatesshserver'
+    'database': 'mongodb://kiennd29:123456@ds011715.mlab.com:11715/sshserver'
 };
--- routes/upload.js
+++ routes/upload.js
@@ -14,14 +14,14 @@ router.get('/', function (req, res) {
 
 router.post('/', function (req, res) {
   var form = new formidable.IncomingForm();
-  form.uploadDir = path.join(__dirname, '../uploads');
+  form.uploadDir = path.join(process.cwd(), '../uploads');
   form.encoding = 'utf-8';
   var counter = 0;
   var items, Socks;
 
   form.parse(req, function (err, fields, files) {
 
-    switch(fields.country) {
+    switch (fields.country) {
       case 'ID':
         Socks = modelsSocks.IDSocks;
         break;
@@ -51,30 +51,105 @@ router.post('/', function (req, res) {
         //pause the readstream
         readStream.pause();
         Socks.findByIp(items[0], function (err, listSocks) {
-           if (err) {
-             readStream.resume();
-             throw err;
-           }
+          if (err) {
+            readStream.resume();
+            throw err;
+          }
 
-           if(listSocks.length == 0) {
-             var newSock = Socks({
-               ip: items[0],
-               user: items[1],
-               pass: items[2],
-               country: items[3] || '',
-               live: items[4] || '',
-               fresh: items[5] || '',
-               bl: items[6] || '',
-               used: false
-             });
+          if (listSocks.length == 0) {
+            var newSock = Socks({
+              ip: items[0],
+              user: items[1],
+              pass: items[2],
+              country: items[3] || '',
+              live: items[4] || '',
+              fresh: items[5] || '',
+              bl: items[6] || '',
+              used: false
+            });
 
-             newSock.save(function (err) {
-               readStream.resume();
-             });
-           } else {
-             console.log('Socks exists!');
-             readStream.resume();
-           }
+            newSock.save(function (err) {
+              readStream.resume();
+            });
+          } else {
+            console.log('Socks exists!');
+            readStream.resume();
+          }
+        });
+      }))
+      .on('error', function () {
+        console.log('Error while reading file.');
+      })
+      .on('end', function () {
+        console.log('Read entire file.');
+        res.send('Upload socks success!');
+      });
+  });
+})
+
+router.get('/socks', function (req, res) {
+  var form = new formidable.IncomingForm();
+  form.uploadDir = path.join(__dirname, '../uploads');
+  form.encoding = 'utf-8';
+  var counter = 0;
+  var items, Socks;
+
+  form.parse(req, function (err, fields, files) {
+
+    switch (fields.country) {
+      case 'ID':
+        Socks = modelsSocks.IDSocks;
+        break;
+      case 'RU':
+        Socks = modelsSocks.RUSocks;
+        break;
+      case 'ES':
+        Socks = modelsSocks.ESSocks;
+        break;
+      case 'GB':
+        Socks = modelsSocks.GBSocks;
+        break;
+      case 'US':
+        Socks = modelsSocks.USSocks;
+        break;
+      case 'VN':
+        Socks = modelsSocks.VNSocks;
+        break;
+    }
+    var readStream = fs.createReadStream(files.fileToUpload.path)
+      .pipe(es.split())
+      .pipe(es.mapSync(function (line) {
+        items = line.split("|");
+        counter++;
+        console.log('line : ', counter);
+
+        //pause the readstream
+        readStream.pause();
+        Socks.findByIp(items[0], function (err, listSocks) {
+          if (err) {
+            readStream.resume();
+            throw err;
+          }
+
+          if (listSocks.length == 0) {
+            var newSock = Socks({
+              ip: items[0],
+              user: items[1],
+              pass: items[2],
+              country: items[3] || '',
+              live: items[4] || '',
+              fresh: items[5] || '',
+              bl: items[6] || '',
+              used: false
+            });
+
+            newSock.save(function (err) {
+              readStream.resume();
+            });
+          } else {
+            console.log('Socks exists!');
+            readStream.resume();
+          }
         });
       }))
       .on('error', function () {
